50dd3a7b6b1b0d3883cb35d25dbc8b1abc3c44a5
vsreyes <virgilio.s.reyes@gmail.com>
2021-05-11 14:19:09 +0200

    Location marker setup

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,5 @@
 import GoogleMapReact from 'google-map-react';
+import LocationMarker from './LocationMarker';
 
 const Map = ({ center, zoom }) => {
   return (
@@ -7,7 +8,9 @@ const Map = ({ center, zoom }) => {
         bootstrapURLKeys={{ key: 'AIzaSyD-hJGdvCAYQEQJ8sx7fVHVQjwsbxQn-Qc' }}
         defaultCenter={center}
         defaultZoom={zoom}
-      ></GoogleMapReact>
+      >
+        <LocationMarker lat={center.lat} lng={center.lng} />
+      </GoogleMapReact>
     </div>
   );
 };
